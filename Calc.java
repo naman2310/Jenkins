270fc33f9f06799f7cd661cbad6fd6d3cc2c5b0f
naman2310 <jainaman2310@gmail.com>
2020-08-07 18:16:08 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -12,7 +12,7 @@ class Calc{
     return a/b;
   }
   public static void main(String []args){
-   // System.out.println(add(10,20));
+    System.out.println(add(10,20));
     System.out.println(sub(10,20));
     System.out.println(mul(10,20));
     System.out.println(div(10,20));                       
